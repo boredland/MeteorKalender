31038ec3456bf7ff6b801f046a44d4b408a59a30
boredland <jo.strassel@gmail.com>
2016-11-02 20:00:08 +0100

    Fix certificate Path.

--- .deploy/mupx_deploy.sh
+++ .deploy/mupx_deploy.sh
@@ -2,6 +2,15 @@
 
 echo "## this is an echo from the script mupx_deploy.sh with the parameter " $1
 npm install -g mupx
+
+ls -l
+
+if [ $1 == master ]
+then
 mupx deploy --config=.deploy/mupx_production.json
-mupx setup --config=.deploy/mupx_test.json
+fi
+
+if [ $1 == test ]
+then
 mupx deploy --config=.deploy/mupx_test.json
+fi
