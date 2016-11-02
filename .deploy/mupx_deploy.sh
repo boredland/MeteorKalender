fe9017e1070856fc90b616f9023e001264fd679b
boredland <jo.strassel@gmail.com>
2016-11-02 23:50:05 +0100

    New changelist

--- .deploy/mupx_deploy.sh
+++ .deploy/mupx_deploy.sh
@@ -2,7 +2,7 @@
 if [ $1 == master ]
 then
 mupx setup --config=.deploy/mupx_production.json
-DEBUG=* mupx deploy --config=.deploy/mupx_production.json
+mupx deploy --config=.deploy/mupx_production.json
 exit $?
 fi
 
