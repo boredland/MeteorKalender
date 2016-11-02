7134be62c1123144802aaf9ef6ab8e9e94bad4da
boredland <jo.strassel@gmail.com>
2016-11-02 20:37:29 +0100

    Try without deployment-script.

--- .deploy/mupx_deploy.sh
+++ .deploy/mupx_deploy.sh
@@ -4,5 +4,3 @@ echo "## this is an echo from the script mupx_deploy.sh with the parameter " $1
 npm install -g mupx
 ls -l
 ls -l .deploy
-if [ $1 == master ];then mupx deploy --config=.deploy/mupx_production.json;fi
-if [ $1 == test ];then mupx deploy --config=.deploy/mupx_test.json;fi
