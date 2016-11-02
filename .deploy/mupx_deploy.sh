7653b8d39eb407dc6a758079cae141abb3c64b21
boredland <jo.strassel@gmail.com>
2016-11-02 18:36:05 +0100

    I think deployments to our production server my work now.

---
+++ .deploy/mupx_deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "## this is an echo from the script mupx_deploy.sh with the parameter " $1
+npm install -g mupx
+#mupx --config=./deploy/mupx-production.json setup
+mupx deploy --config=.deploy/mupx_production.json
--- mup_deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "## this is an echo from the script mup_deploy.sh"
-npm install -g mup ssh2
-mup setup
-mup deploy
