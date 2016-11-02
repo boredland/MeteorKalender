0989912d75eb317957d93a4c78b5ea46735b5db3
boredland <jo.strassel@gmail.com>
2016-11-02 20:07:44 +0100

    Remove Heroku and testing for speed up in deployment-test, check if it now only deploys master.

--- .deploy/mupx_deploy.sh
+++ .deploy/mupx_deploy.sh
@@ -2,15 +2,6 @@
 
 echo "## this is an echo from the script mupx_deploy.sh with the parameter " $1
 npm install -g mupx
-
 ls -l
-
-if [ $1 == master ]
-then
-mupx deploy --config=.deploy/mupx_production.json
-fi
-
-if [ $1 == test ]
-then
-mupx deploy --config=.deploy/mupx_test.json
-fi
+if [ $1 == master ];then mupx deploy --config=.deploy/mupx_production.json;fi
+if [ $1 == test ];then mupx deploy --config=.deploy/mupx_test.json;fi
