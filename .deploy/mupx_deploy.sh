1633e2c4b84f7b5b68baa1cd2449ece2a6345840
boredland <jo.strassel@gmail.com>
2016-11-06 17:23:11 +0100

    Fix input-checks.

--- .deploy/mupx_deploy.sh
+++ .deploy/mupx_deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if [ $1 == master ]
 then
-#mupx setup --config=.deploy/mupx_production.json
+mupx setup --config=.deploy/mupx_production.json
 mupx deploy --config=.deploy/mupx_production.json
 exit $?
 fi
 
 if [ $1 == test ]
 then
-#mupx setup --config=.deploy/mupx_test.json
+mupx setup --config=.deploy/mupx_test.json
 mupx deploy --config=.deploy/mupx_test.json
 exit $?
 fi
