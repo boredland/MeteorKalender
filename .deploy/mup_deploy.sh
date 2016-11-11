d30e8e52accb86e0311c718ecc49fb4221d5dc16
Jonas Strassel <jo.strassel@gmail.com>
2016-11-11 17:40:04 +0100

    Added packages for the new Meteor Version. Fixed deployment-notifications hopefully.

--- .deploy/mup_deploy.sh
+++ .deploy/mup_deploy.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
+slack_it (){
+curl -X POST --data-urlencode 'payload={"channel": "'$channel'", "username": "Meteor-UP", "text": "'$RESULT_MESSAGE'"}' https://hooks.slack.com/services/$webhook_key
+}
+
+check_sucess () {
+if [[ $MUP_RESULT == *"Verifying Deployment: SUCCESS"* ]]
+then
+  RESULT_MESSAGE="Sucessfully deployed <'$DESTINATION_URL'|#'$TRAVIS_BUILD_NUMBER'>.";
+else
+  RESULT_MESSAGE="Deployment for #'$TRAVIS_BUILD_NUMBER' failed. Reverted back to previous version on '$DESTINATION_URL'"
+fi
+}
+
+deploy () {
+MUP_RESULT=$(mup deploy | tail -n1)
+echo $MUP_RESULT
+check_sucess
+slack_it
+}
+
 if [ $1 == master ]
 then
 cd .deploy/production
-MUP_RESULT=$(mup deploy | tail -n1)
-echo $MUP_RESULT
-curl -X POST --data-urlencode 'payload={"channel": "#info_002_cd_mup_prod", "username": "Meteor-UP", "text": "Sucessfully deployed <https://meteorkalender.freeddns.org|#'$TRAVIS_BUILD_NUMBER'>. Result: '$MUP_RESULT'"}' https://hooks.slack.com/services/$Webhook_Production
+channel="#info_002_cd_mup_prod"
+DESTINATION_URL="https://test.meteorkalender.freeddns.org"
+webhook_key=$Webhook_Test
+deploy
 fi
 
 if [ $1 == test ]
 then
 cd .deploy/test
-MUP_RESULT=$(mup deploy | tail -n1)
-echo $MUP_RESULT
-curl -X POST --data-urlencode 'payload={"channel": "#info_002_cd_mup_prod", "username": "Meteor-UP", "text": "Sucessfully deployed <https://test.meteorkalender.freeddns.org|#'$TRAVIS_BUILD_NUMBER'>. Result: '$MUP_RESULT'"}' https://hooks.slack.com/services/$Webhook_Test
+channel="#info_002_cd_mup_test"
+DESTINATION_URL="https://meteorkalender.freeddns.org"
+webhook_key=$Webhook_Test
+deploy
 fi
