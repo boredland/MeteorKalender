f1951a51c7a989b94994f95865a5219d11bc3256
Jonas Strassel <jo.strassel@gmail.com>
2016-11-11 17:15:32 +0100

    Upgrade to meteor 1.4.2.1 Fix *exploding* curl.

--- .deploy/mup_deploy.sh
+++ .deploy/mup_deploy.sh
@@ -2,13 +2,15 @@
 if [ $1 == master ]
 then
 cd .deploy/production
-MUP_OUTPUT=$(mup deploy 2>&1)
-curl -X POST --data-urlencode 'payload={"channel": "#info_002_cd_mup_prod", "username": "Meteor-UP", "text": "Sucessfully deployed <https://meteorkalender.freeddns.org|#'$TRAVIS_BUILD_NUMBER'>. Log: '$MUP_OUTPUT'"}' https://hooks.slack.com/services/$Webhook_Production
+MUP_RESULT=$(mup deploy | tail -n1)
+echo $MUP_RESULT
+curl -X POST --data-urlencode 'payload={"channel": "#info_002_cd_mup_prod", "username": "Meteor-UP", "text": "Sucessfully deployed <https://meteorkalender.freeddns.org|#'$TRAVIS_BUILD_NUMBER'>. Result: '$MUP_RESULT'"}' https://hooks.slack.com/services/$Webhook_Production
 fi
 
 if [ $1 == test ]
 then
 cd .deploy/test
-MUP_OUTPUT=$(mup deploy 2>&1)
-curl -X POST --data-urlencode 'payload={"channel": "#info_002_cd_mup_prod", "username": "Meteor-UP", "text": "Sucessfully deployed <https://test.meteorkalender.freeddns.org|#'$TRAVIS_BUILD_NUMBER'>. Log: '$MUP_OUTPUT'"}' https://hooks.slack.com/services/$Webhook_Test
+MUP_RESULT=$(mup deploy | tail -n1)
+echo $MUP_RESULT
+curl -X POST --data-urlencode 'payload={"channel": "#info_002_cd_mup_prod", "username": "Meteor-UP", "text": "Sucessfully deployed <https://test.meteorkalender.freeddns.org|#'$TRAVIS_BUILD_NUMBER'>. Result: '$MUP_RESULT'"}' https://hooks.slack.com/services/$Webhook_Test
 fi
