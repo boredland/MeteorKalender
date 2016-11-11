ad9ccaafc29e755cfc27a66a760a572087cafb28
Jonas Strassel <jo.strassel@gmail.com>
2016-11-11 17:01:29 +0100

    Added slack-integration for mup. Added logs, didn't test if that works, but it *should*. Contrary to the statement it currently doesn't check if the deploy was successful.

--- .deploy/mup_deploy.sh
+++ .deploy/mup_deploy.sh
@@ -2,13 +2,13 @@
 if [ $1 == master ]
 then
 cd .deploy/production
-#mup reconfig
-mup deploy
-exit $?
+MUP_OUTPUT=$(mup deploy 2>&1)
+curl -X POST --data-urlencode 'payload={"channel": "#info_002_cd_mup_prod", "username": "Meteor-UP", "text": "Sucessfully deployed <https://meteorkalender.freeddns.org|#'$TRAVIS_BUILD_NUMBER'>. Log: '$MUP_OUTPUT'"}' https://hooks.slack.com/services/$Webhook_Production
 fi
+
 if [ $1 == test ]
 then
 cd .deploy/test
-mup deploy
-exit $?
+MUP_OUTPUT=$(mup deploy 2>&1)
+curl -X POST --data-urlencode 'payload={"channel": "#info_002_cd_mup_prod", "username": "Meteor-UP", "text": "Sucessfully deployed <https://test.meteorkalender.freeddns.org|#'$TRAVIS_BUILD_NUMBER'>. Log: '$MUP_OUTPUT'"}' https://hooks.slack.com/services/$Webhook_Test
 fi
