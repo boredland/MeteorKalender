cd7725ed2b599fe1ddc0cd3e1a9cae5bef862609
Furkan Yücel <mfy1992@live.de>
2016-11-11 14:02:35 +0100

    try to add tasks

--- client/views/home_public/home_public.js
+++ client/views/home_public/home_public.js
@@ -15,3 +15,24 @@ Template.HomePublic.helpers({
         return Test.find({});
     }
 });
+
+//Try to add new Task
+Template.HomePublic.events({
+    'submit .new-tas'(event) {
+        // Prevent default browser submit
+        event.preventDefault();
+
+        // Get value from form element
+        const target = event.target;
+        const text = target.text.value;
+
+        // insert a task into the collection
+        Task.insert({
+            createdAt: new Date(),
+            text, //current time
+        });
+
+        //clear from
+        target.text.value = '';
+    }
+});
