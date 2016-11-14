e5e82158a7875d187a2bf7e1260bb8f46540a4ce
boredland <jo.strassel@gmail.com>
2016-11-14 19:15:31 +0100

    Added collection for availabilities to private home.

--- both/collections/availabilities.js
+++ both/collections/availabilities.js
@@ -2,4 +2,4 @@
  * Created by jonasstr on 14.11.16.
  */
 export const Availabilities = new Mongo.Collection("availabilities");
-Availabilities.insert({userId: "this is a userid",startDate: "this is a startdate",endDate: "this is an enddate",categoryId: "this is a cat_id"});
+//Availabilities.insert({userId: "MFS5YMFqpTPMZ3dgC",startDate: "2020-01-19",endDate: "2020-01-19",categoryId: "this is a cat_id"});
--- client/views/home_private/home_private.js
+++ client/views/home_private/home_private.js
@@ -1,18 +1,33 @@
 import {Availabilities} from '/both/collections/availabilities'
-var avail = Availabilities.find().fetch();
-
+var pageSession = new ReactiveDict();
+var avail = Availabilities.find({userId:Meteor.userId()}).fetch();
+Meteor.subscribe('availabilities', 'ru_RU');
 Template.HomePrivate.rendered = function() {
+
 };
 
 Template.HomePrivate.events({
-	
+    "submit": function(e, t) {
+        e.preventDefault();
+        pageSession.set("availabilitiesInsertInsertFormInfoMessage", "");
+        pageSession.set("availabilitiesInsertInsertFormErrorMessage", "");
+        Availabilities.insert({
+            userId: Meteor.userId(),
+            startDate: new Date(t.find('#startdate').value.trim()),
+            endDate: new Date(t.find('#enddate').value.trim()),
+            categoryId: t.find('#categoryid').value.trim()
+        });
+    }
 });
 
 Template.HomePrivate.helpers({
+    "infoMessage": function() {
+        return pageSession.get("availabilitiesInsertInsertFormInfoMessage");
+    },
+    "errorMessage": function() {
+        return pageSession.get("availabilitiesInsertInsertFormErrorMessage");
+    },
     availabilities(){
         return avail;
-    },
-    userid() {
-        return Meteor.userId();
     }
 });
