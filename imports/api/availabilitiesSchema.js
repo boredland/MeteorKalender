555706745de08d200ac8b291221d71e1f0462c8a
boredland <jo.strassel@gmail.com>
2016-12-04 13:14:17 +0100

    Added enabledHours, so you can't change the Date using the timepickers.

--- imports/api/availabilitiesSchema.js
+++ imports/api/availabilitiesSchema.js
@@ -65,6 +65,7 @@ export var availabilitiesFormSchema = new SimpleSchema({
                     locale: 'de',
                     format: 'LT',
                     stepping: 5,
+                    enabledHours: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
                 }
             }
         },
@@ -89,6 +90,7 @@ export var availabilitiesFormSchema = new SimpleSchema({
                     locale: 'de',
                     format: 'LT',
                     stepping: 5,
+                    enabledHours: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
                 }
             }
         },
