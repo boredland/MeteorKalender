706f6cca76d877b9d43d0bedd5ac7eca721590c9
Furkan Yücel <mfy1992@live.de>
2016-12-06 18:08:15 +0100

    event hinzugefuegt

--- client/views/home_private/calendars/edit_calendar/edit_calendar.js
+++ client/views/home_private/calendars/edit_calendar/edit_calendar.js
@@ -21,11 +21,41 @@ Template.EditCalendar.created = function() {
 };
 
 Template.EditCalendar.events({
-
+    "click #dataview-delete-button": function(e) {
+        e.preventDefault();
+        bootbox.dialog({
+            message: "Delete? Are you sure?",
+            title: "Delete",
+            animate: false,
+            buttons: {
+                success: {
+                    label: "Yes",
+                    className: "btn-success",
+                    callback: function() {
+                        Meteor.call('calendars.remove', getCurrentCalendarId());
+                        Router.go('home_private.calendars');
+                    }
+                },
+                danger: {
+                    label: "No",
+                    className: "btn-default"
+                }
+            }
+        });
+        return false;
+    }
 });
 
 Template.EditCalendar.helpers({
     getCalendars(){
         return Calendars.find();
+    }
+});
+
+AutoForm.hooks({
+    calendarEditForm: {
+        onSuccess: function() {
+            Router.go('home_private.calendars');
+        }
     }
 });
