5e44b1183815a432655b727fa7ed2bffb50291fe
Sebastian Blam <sebastian.blam@gmail.com>
2016-12-11 21:42:29 +0100

    added winston logger and example

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 import '/imports/api/availabilitiesCollection'
 //Import creates the collection on the server.
 var verifyEmail = true;
+var logger = require('winston');
 
 Accounts.config({ sendVerificationEmail: verifyEmail });
 
 Meteor.startup(function() {
-
+	logger.info('Meteor started');
 	// read environment variables from Meteor.settings
 	if(Meteor.settings && Meteor.settings.env && _.isObject(Meteor.settings.env)) {
 		for(var variableName in Meteor.settings.env) {
