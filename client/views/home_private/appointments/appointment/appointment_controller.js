2fe5741f1c87e579241c09a06efaad888127eb44
boredland <jo.strassel@gmail.com>
2016-12-12 16:42:05 +0100

    Add page for sinlge Appointment.

---
+++ client/views/home_private/appointments/appointment/appointment_controller.js
@@ -0,0 +1,43 @@
+this.AppointmentController = RouteController.extend({
+    template: "Appointment",
+
+    onBeforeAction: function() {
+        this.next();
+    },
+
+    action: function() {
+        this.render("HomePrivate");
+        if(this.isReady()) { this.render("Appointment", { to: "HomePrivateSubcontent"}); } else { this.render("HomePrivate"); this.render("loading", { to: "HomePrivateSubcontent" });}
+        /*ACTION_FUNCTION*/
+    },
+
+    isReady: function() {
+
+
+        var subs = [
+        ];
+        var ready = true;
+        _.each(subs, function(sub) {
+            if(!sub.ready())
+                ready = false;
+        });
+        return ready;
+    },
+
+    data: function() {
+
+
+        var data = {
+            params: this.params || {}
+        };
+
+
+
+
+        return data;
+    },
+
+    onAfterAction: function() {
+
+    }
+});
