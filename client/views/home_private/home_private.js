0f61592a7a7230425b778ea490395ce1e847377e
Sebastian Blam <sebastian.blam@gmail.com>
2016-12-16 16:26:25 +0100

    removed last stuff  from HomePrivateSideMenu

--- client/views/home_private/home_private.js
+++ client/views/home_private/home_private.js
@@ -4,26 +4,3 @@ Template.HomePrivate.events({});
 
 //function is called within home_private.html
 Template.HomePrivate.helpers({});
-
-Template.HomePrivateSideMenu.rendered = function() {
-    $(".menu-item-collapse .dropdown-toggle").each(function() {
-        if($(this).find("li.active")) {
-            $(this).removeClass("collapsed");
-        }
-        $(this).parent().find(".collapse").each(function() {
-            if($(this).find("li.active").length) {
-                $(this).addClass("in");
-            }
-        });
-    });
-
-};
-
-Template.HomePrivateSideMenu.events({
-    "click .toggle-text": function(e, t) {
-        e.preventDefault();
-        $(e.target).closest("ul").toggleClass("menu-hide-text");
-    }
-});
-
-Template.HomePrivateSideMenu.helpers({});
