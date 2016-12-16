c26fb44a6b485323bde6a8dd98233f651e18e660
Jonas Strassel <jo.strassel@gmail.com>
2016-12-16 18:59:45 +0100

    Corrected schema, introduced at least a little bit of modularity.

--- imports/api/availabilitiesSchema.js
+++ imports/api/availabilitiesSchema.js
@@ -3,61 +3,155 @@
  */
 import {Calendars} from '/imports/api/calendarsCollection';
 
+var checkDate = function (start,end) {
+    var start = moment(start);
+    var end = moment(end);
+    if (start >= end) {
+        return 'startTimeAfterEnd';
+    };
+    if (start.get('h') == end.get('h') && start.get('m') == end.get('m')){
+        return 'sameTime';
+    };
+    if (start < moment()){
+        return 'inThePast'
+    };
+};
+
+var checkDateAndTime = function(startTime,endTime,date){
+    var startTime = moment(startTime);
+    var endTime = moment(endTime);
+    var startDate = moment(new Date(date)).hour(startTime.get('h')).minute(startTime.get('m'));
+    var endDate = moment(new Date(date)).hour(endTime.get('h')).minute(endTime.get('m'));
+    return checkDate(startDate,endDate);
+};
+
+var checkDuration = function (start_in,end_in,chunkDuration_in) {
+    var starttime = moment(new Date(start_in));
+    var endtime = moment(new Date(end_in));
+    var duration = Math.round((moment(endtime)-moment(starttime))/(1000*60));//|0; //<-- das ist die duration in minuten
+    var chunkDuration = chunkDuration_in;
+    if ((duration > 0) && (duration < chunkDuration)){
+        return 'durationSmaller';
+    };
+    if ((duration%chunkDuration) != 0) {
+        return 'durationNotMultiple';
+    };
+};
+
 SimpleSchema.messages({
-    'startTimeAfterEnd': 'The start-time is after the end-time',
-    'endTimeBeforeStart': 'The end-time is before the start-time',
+    'startTimeAfterEnd': 'The start is after the end-time',
+    'inThePast': "The start-time is in the past",
     'durationSmaller': 'The duration of your consultation hour is smaller than the chunk-period you selected',
     'durationNotMultiple': 'The duration of your consultation hour is not a multiple of the chunk-period you selected',
     'sameTime': 'Start- and Endtime are the same',
 });
 
-// This schema validates the insertion.
+// This schema validates the insertions and the edit-page-form
 export var availabilitiesSchema = new SimpleSchema({
     userId: {
-        type: String
+        type: String,
+        autoform: {
+            type: "hidden",
+            label: false
+        }
     },
     startDate: {
-        type: Date
+        type: Date,
+        autoform: {
+            afFieldInput: {
+                class: "startdate",
+                type: "bootstrap-datetimepicker",
+                dateTimePickerOptions: {
+                    sideBySide: true,
+                    inline: true,
+                    locale: 'de',
+                    stepping: 5,
+                    enabledHours: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
+                    minDate: new Date(),
+                }
+            }
+        },
+        custom: function() {
+            return checkDate(this.field("startDate").value,this.field("endDate").value);
+        }
     },
     endDate: {
-        type: Date
+        type: Date,
+        autoform: {
+            afFieldInput: {
+                class: "enddate",
+                type: "bootstrap-datetimepicker",
+                dateTimePickerOptions: {
+                    sideBySide: true,
+                    inline: true,
+                    locale: 'de',
+                    stepping: 5,
+                    enabledHours: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
+                    minDate: new Date(),
+                }
+            }
+        },
+        custom: function() {
+            return checkDate(this.field("startDate").value,this.field("endDate").value);
+        }
     },
     familyId: {
-        type: String
-    },
-    calendarId: {
-        type: Array
-    },
-    'calendarId.$': {
-        type: String
+        type: String,
+        autoform: {
+            type: "hidden",
+            label: false,
+        }
     },
     bookedByDate: {
         type: Date,
-        optional: true
+        optional: true,
+        autoform: {
+            type: "hidden",
+        }
     },
     bookedByConfirmed: {
         type: Boolean,
-        optional: true
+        optional: true,
+        autoform: {
+            type: "hidden",
+        }
     },
+    // das ist Käse.
     bookedByReserved: {
         type: Boolean,
-        optional: true
+        optional: true,
+        autoform: {
+            type: "hidden",
+        }
     },
     bookedByName: {
         type: String,
-        optional: true
+        optional: true,
+        autoform: {
+            type: "hidden",
+        }
+
     },
     bookedByEmail: {
         type: String,
-        optional: true
+        optional: true,
+        autoform: {
+            type: "hidden",
+        }
     },
     bookedByConfirmationToken: {
         type: String,
-        optional: true
+        optional: true,
+        autoform: {
+            type: "hidden",
+        }
     },
     bookedByCancellationToken: {
         type: String,
-        optional: true
+        optional: true,
+        autoform: {
+            type: "hidden",
+        }
     }
 });
 
@@ -66,7 +160,7 @@ export var availabilitiesFormSchema = new SimpleSchema({
     startDate: {
         type: Date,
         autoform: {
-            value: new Date(moment().set(0,'ms').set(0,'s')),
+            value: new Date(moment().seconds(0)),
             afFieldInput: {
                 class: "startdate",
                 type: "bootstrap-datetimepicker",
@@ -98,20 +192,13 @@ export var availabilitiesFormSchema = new SimpleSchema({
             }
         },
         custom: function() {
-            var starttime = moment(new Date(this.field("startTime").value));
-            var endtime = moment(new Date(this.field("endTime").value));
-            if (starttime >= endtime) {
-                return 'startTimeAfterEnd';
-            }
-            if (starttime.get('h') == endtime.get('h') && starttime.get('m') == endtime.get('m')){
-                return 'sameTime';
-            }
+            return checkDateAndTime(this.field("startTime").value,this.field("endTime").value,this.field("startDate").value);
         }
     },
     endTime: {
         type: Date,
         autoform: {
-            value: new Date(moment().set(0,'ms').add(10,'m').set(0,'s')),
+            value: new Date(moment().set(0,'ms').set(0,'s').add(10,'m')),
             afFieldInput: {
                 class: "endtime",
                 type:  "bootstrap-datetimepicker",
@@ -126,14 +213,7 @@ export var availabilitiesFormSchema = new SimpleSchema({
             }
         },
         custom: function() {
-            var starttime = moment(new Date(this.field("startTime").value));
-            var endtime = moment(new Date(this.field("endTime").value));
-            if (starttime >= endtime) {
-                return 'endTimeBeforeStart';
-            }
-            if (starttime.get('h') == endtime.get('h') && starttime.get('m') == endtime.get('m')){
-                return 'sameTime';
-            }
+            return checkDateAndTime(this.field("startTime").value,this.field("endTime").value,this.field("startDate").value);
         }
     },
     chunkDuration: {
@@ -144,20 +224,7 @@ export var availabilitiesFormSchema = new SimpleSchema({
             defaultValue: 10,
         },
         custom: function() {
-            var starttime = moment(new Date(this.field("startTime").value));
-            var endtime = moment(new Date(this.field("endTime").value));
-            var duration = Math.round((moment(endtime)-moment(starttime))/(1000*60));//|0; //<-- das ist die duration in minuten
-            //console.log(duration);
-
-            var chunkDuration = this.field("chunkDuration").value;
-
-            if ((duration > 0) && (duration < chunkDuration)){
-                return 'durationSmaller';
-            }
-
-            if ((duration%chunkDuration) != 0) {
-                return 'durationNotMultiple';
-            }
+            return checkDuration(this.field("startTime").value,this.field("endTime").value,this.field("chunkDuration").value);
         }
     },
     dontSkipHolidays:{
@@ -221,7 +288,7 @@ export var availabilitiesFormSchema = new SimpleSchema({
     }
 });
 
-
+// This validates the Booking-Form
 export var bookingFormSchema = new SimpleSchema({
     availabilityId: {
         type: String,
