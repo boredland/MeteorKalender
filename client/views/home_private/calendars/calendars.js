88ace344035b32f1316843ad182f8e5fde64eff8
Hitbox47 <m.tekcam@icloud.com>
2016-12-21 09:57:19 +0100

    Kalender Lösch funktion funktioniert wieder.

--- client/views/home_private/calendars/calendars.js
+++ client/views/home_private/calendars/calendars.js
@@ -10,10 +10,12 @@ Template.Calendars.onCreated(function bodyOnCreated() {
 Template.Calendars.events({
     "change #published": (function(event, template) {
         var status = event.target.checked;
+
         Meteor.call('calendars.updatePublishedState',this._id,status);
     }),
     "click #delete-button": function(e) {
         e.preventDefault();
+        var me = this;
         bootbox.dialog({
             message: "Do you want to delete this calendar?",
             title: "Delete calendar",
