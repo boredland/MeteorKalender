c0775700ffee717d71dd194dae947dc34c0463dc
boredland <jo.strassel@gmail.com>
2017-01-16 13:21:51 +0100

    Do them apks work noooowwwwwwwwwww?

--- .app/generate_apks.sh
+++ .app/generate_apks.sh
@@ -4,8 +4,17 @@ echo "Set Android Home and path for platform tools"
 export ANDROID_HOME="$HOME/Android/Sdk"
 export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 echo "Build dev-apk"
-/usr/local/bin/meteor build . --server https://meteorkalender.herokuapp.com:443 && mv ./android/release-unsigned.apk ./android/dateyourprof-unsigned_dev.apk
+## keytool -genkey -alias dateyourprof -keyalg RSA -keysize 2048 -validity 10000
+/usr/local/bin/meteor build . --server https://meteorkalender.herokuapp.com:443
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 ./android/release-unsigned.apk dateyourprof
+mv ./android/release-unsigned.apk ./android/dateyourprof_dev.apk
+
 echo "Build test-apk"
-/usr/local/bin/meteor build . --server https://test.meteorkalender.freeddns.org:443 && mv ./android/release-unsigned.apk ./android/dateyourprof-unsigned_test.apk
+/usr/local/bin/meteor build . --server https://test.meteorkalender.freeddns.org:443
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 ./android/release-unsigned.apk dateyourprof
+mv ./android/release-unsigned.apk ./android/dateyourprof_test.apk
+
 echo "Build prod-apk"
-/usr/local/bin/meteor build . --server https://meteorkalender.freeddns.org:443 && mv ./android/release-unsigned.apk ./android/dateyourprof-unsigned_prod.apk
+/usr/local/bin/meteor build . --server https://meteorkalender.freeddns.org:443
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 ./android/release-unsigned.apk dateyourprof
+mv ./android/release-unsigned.apk ./android/dateyourprof_prod.apk
