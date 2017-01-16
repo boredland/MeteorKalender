655ccd4abe649f318f223460505df9b23060d2ef
boredland <jo.strassel@gmail.com>
2017-01-16 12:14:03 +0100

    Add some debugging-output for the bash-script.

--- .app/generate_apks.sh
+++ .app/generate_apks.sh
@@ -4,8 +4,8 @@ echo "Set Android Home and path for platform tools"
 export ANDROID_HOME="$HOME/Android/Sdk"
 export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 echo "Build dev-apk"
-/usr/local/bin/meteor build ./.app/ --server https://meteorkalender.herokuapp.com:443 && mv ./.app/android/release-unsigned.apk ./.app/android/release-unsigned_dev.apk
+/usr/local/bin/meteor build . --server https://meteorkalender.herokuapp.com:443 && mv ./android/release-unsigned.apk ./android/dateyourprof-unsigned_dev.apk
 echo "Build test-apk"
-/usr/local/bin/meteor build ./.app/ --server https://test.meteorkalender.freeddns.org:443 && mv ./.app/android/release-unsigned.apk ./.app/android/release-unsigned_test.apk
+/usr/local/bin/meteor build . --server https://test.meteorkalender.freeddns.org:443 && mv ./android/release-unsigned.apk ./android/dateyourprof-unsigned_test.apk
 echo "Build prod-apk"
-/usr/local/bin/meteor build ./.app/ --server https://meteorkalender.freeddns.org:443 && mv ./.app/android/release-unsigned.apk ./.app/android/release-unsigned_prod.apk
+/usr/local/bin/meteor build . --server https://meteorkalender.freeddns.org:443 && mv ./android/release-unsigned.apk ./android/dateyourprof-unsigned_prod.apk
