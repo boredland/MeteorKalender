99a96976ba9f758557f36c03e0f8dbfc459f603d
boredland <jo.strassel@gmail.com>
2017-01-17 16:46:55 +0100

    Build signed and working app.

--- get_target.sh
+++ get_target.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 ## this does work on my computer.
-if [$1 == "master"]; then
+if [ $1 == master ]
+then
 echo "https://meteorkalender.freeddns.org:443"
-else if [$1 == test]; then
+elif [ $1 == test ]
+then
 echo "https://test.meteorkalender.freeddns.org:443"
-else echo "https://meteorkalender.herokuapp.com:443";fi
+else
+echo "https://meteorkalender.herokuapp.com:443"
 fi
