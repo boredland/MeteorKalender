06ce7154a7e881cb39a595c57089f0b4f15543e0
Sebastian Blam <sebastian.blam@gmail.com>
2017-01-18 17:38:50 +0100

    added apk download notice for android devices (used master002 apk here)

--- client/views/home_public/home_public.js
+++ client/views/home_public/home_public.js
@@ -9,3 +9,7 @@ Template.HomePublic.onCreated(function bodyOnCreated() {
 Template.HomePublic.helpers({
 
 });
+
+Template.registerHelper('isAndroid',function(){
+    return navigator.userAgent.toLowerCase().indexOf("android") > -1;
+});
