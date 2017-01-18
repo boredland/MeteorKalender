d83bdd9d66b61a2f16151221f6762727574d5035
Sebastian Blam <sebastian.blam@gmail.com>
2017-01-18 18:09:48 +0100

    bugfix for android device detection

--- client/views/home_public/home_public.js
+++ client/views/home_public/home_public.js
@@ -7,9 +7,7 @@ Template.HomePublic.onCreated(function bodyOnCreated() {
 });
 
 Template.HomePublic.helpers({
-
-});
-
-Template.registerHelper('isAndroid',function(){
-    return navigator.userAgent.toLowerCase().indexOf("android") > -1;
+    isAndroid: function () {
+        return navigator.userAgent.toLowerCase().indexOf("android") > -1;
+    }
 });
