62248f6e83f75ea5a67fec41037e0d99b60982c3
Furkan Yücel <mfy1992@live.de>
2017-01-23 23:04:36 +0100

    verify_email function

--- client/views/verify_resend/verify_resend.js
+++ client/views/verify_resend/verify_resend.js
@@ -5,31 +5,31 @@ Template.VerifyResend.rendered = function() {
 	
 	$("input[autofocus]").focus();
 };
-/*
-Template.Login.events({
-	"submit #login_form": function(e, t) {
+
+Template.VerifyResend.events({
+	"submit #verify_form": function(e, t) {
 		e.preventDefault();
 		nullMessages(pageSession);
 
 		var submit_button = $(t.find(":submit"));
 
-		var login_email = t.find('#login_email').value.trim();
+		var verify_email = t.find('#verify_email').value.trim();
 
         // check email
-        if(!isValidEmail(register_email))
+        if(!isValidEmail(verify_email))
         {
             setErrorMessage(pageSession, "Please enter valid e-mail address that ends with .fra-uas.de, .frankfurt-unversity.de or fh-frankfurt.de.");
-            t.find('#register_email').focus();
+            t.find('#verify_email').focus();
             return false;
         }
 
         submit_button.button("loading");
-        Accounts.sendVerificationEmail(_userId,"login_email");
+        Accounts.sendVerificationEmail(_userId,"verify_email");
 
 	},
 });
 
-Template.Login.helpers({
+Template.VerifyResend.helpers({
     getPageSession: function () {
         return pageSession;
     },
@@ -39,4 +39,3 @@ Template.Login.helpers({
     }
 	
 });
-*/
